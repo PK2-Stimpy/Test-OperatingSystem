caf038034203b3e3345684e679949708a945ea33
PK2_Stimpy <thefernan1@gmail.com>
2021-06-10 23:29:05 +0200

    Header and init files

---
+++ src/impl/x86_64/boot/header.asm
@@ -0,0 +1,11 @@
+section .multiboot_header
+header_start:
+	dd 0xe85250d6 ; multiboot2 number
+	dd 0 ; architecture protected mode i386
+	dd header_end-header_start ; header length
+	dd 0x100000000 - (0xe85250d6+0+(header_end-header_start)) ; checksum
+	
+	dw 0
+	dw 0
+	dd 8
+header_end:
