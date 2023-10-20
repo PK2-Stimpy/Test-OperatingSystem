bb5b389af3a9c5201ba7bf0ac860faf539d7e5e5
PK2_Stimpy <pk2stimpy@furryporn.fun>
2023-10-20 20:15:12 +0200

    bored tbh

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,5 +3,11 @@
 void kernel_main() {
 	print_clear();
 	print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-	print_str("This is a test!");
+	print_str("Bienvenido a VinedoOS! (by pk2)");
+	print_newline();
+	print_str("El mejor sistema operativo en honor a Vinedo Bravo!!!!!");
+	print_newline();
+	print_newline();
+	print_newline();
+	print_str("lol");
 }
